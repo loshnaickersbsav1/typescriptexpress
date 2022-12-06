b18f72ed1a7b2293c568984f5cf235b44e8f84aa
losh Naicker <losh.Naicker@liberty-it.co.uk>
2022-12-06 02:56:44 +0000

    db query

--- src/services/business.capabilties.service.ts
+++ src/services/business.capabilties.service.ts
@@ -1,10 +1,9 @@
 import { db } from './connection';
+
 import { Application } from 'interfaces/applications.interface';
 class BusinessCapabilityService {
   public async findAllApplications(): Promise<Application[]> {
-    return await db.query(
-      'select application from public.ENTERPRISE_TROUX_APPLICATION_TO_BUSINESS_TAXONOMY where capability = "Corporate Functions"',
-    );
+    return await db.query('select application from public."ENTERPRISE_TROUX_APPLICATION_TO_BUSINESS_TAXONOMY" limit 20 offset 50');
   }
 }
 export default BusinessCapabilityService;
